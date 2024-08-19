fa6841130586973c764e29676b70c799627b74de
leo-yssa <yssa@kakao.com>
2024-08-19 13:19:56 +0900

    add axios

--- src/components/Button/Deploy.tsx
+++ src/components/Button/Deploy.tsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+import instance from "../../lib/rest";
 import { HashConnectConnectionState } from "hashconnect";
-import {
-  ContractFunctionParameters,
-  ContractExecuteTransaction,
-  ContractId,
-  Hbar,
-} from "@hashgraph/sdk";
+// import {
+//   ContractFunctionParameters,
+//   ContractExecuteTransaction,
+//   ContractId,
+//   Hbar,
+// } from "@hashgraph/sdk";
 
 const Deploy = () => {
   const accountId = useSelector((state: RootState) => state.wallet.accountId);
@@ -25,32 +26,38 @@ const Deploy = () => {
       console.log(hashconnectState);
       return;
     }
-    const metadata =
-      "ipfs://bafyreie3ichmqul4xa7e6xcy34tylbuq2vf3gnjf7c55trg3b6xyjr4bku/metadata.json";
-
-    const contractId: ContractId = ContractId.fromString("0.0.4673808");
-
-    // Mint NFT
-    const mintToken = new ContractExecuteTransaction()
-      .setContractId(contractId)
-      .setGas(4000000)
-      .setPayableAmount(new Hbar(50))
-      // .setMaxTransactionFee(new Hbar(20)) //Use when HBAR is under 10 cents
-      .setFunction(
-        "registerToilet",
-        new ContractFunctionParameters()
-          .addString("test") // Token address
-          .addString("test")
-          .addString("memo")
-          .addInt64(2)
-          .addString(metadata)
-      );
-    const mintTokenTxawait = await hashconnect.sendTransaction(
-      accountId,
-      mintToken
+    console.log(
+      await instance({
+        method: "GET",
+        url: "/api",
+      })
     );
+    // const metadata =
+    //   "ipfs://bafyreie3ichmqul4xa7e6xcy34tylbuq2vf3gnjf7c55trg3b6xyjr4bku/metadata.json";
 
-    console.log(mintTokenTxawait);
+    // const contractId: ContractId = ContractId.fromString("0.0.4673808");
+
+    // // Mint NFT
+    // const mintToken = new ContractExecuteTransaction()
+    //   .setContractId(contractId)
+    //   .setGas(4000000)
+    //   .setPayableAmount(new Hbar(50))
+    //   // .setMaxTransactionFee(new Hbar(20)) //Use when HBAR is under 10 cents
+    //   .setFunction(
+    //     "registerToilet",
+    //     new ContractFunctionParameters()
+    //       .addString("test") // Token address
+    //       .addString("test")
+    //       .addString("memo")
+    //       .addInt64(2)
+    //       .addString(metadata)
+    //   );
+    // const mintTokenTxawait = await hashconnect.sendTransaction(
+    //   accountId,
+    //   mintToken
+    // );
+
+    // console.log(mintTokenTxawait);
   };
   return <button onClick={deploy}>Deploy</button>;
 };
--- src/containers/Home/HomeContainer.tsx
+++ src/containers/Home/HomeContainer.tsx
@@ -1,21 +1,18 @@
-import { Component, ReactNode } from "react";
 import ListOfRestrooms from "../../components/Layout/ListOfToilets";
 import Deploy from "../../components/Button/Deploy";
 
-class HomeContainer extends Component {
-  render(): ReactNode {
-    return (
-      <>
-        <div>
-          <Deploy />
-        </div>
-        <br></br>
-        <div>
-          <ListOfRestrooms items={[]} />
-        </div>
-      </>
-    );
-  }
-}
+const HomeContainer = () => {
+  return (
+    <>
+      <div>
+        <Deploy />
+      </div>
+      <br></br>
+      <div>
+        <ListOfRestrooms items={[]} />
+      </div>
+    </>
+  );
+};
 
 export default HomeContainer;
